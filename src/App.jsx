1f2c40f5d4fec0f7a62799b644e4ca951ade1c1a
jana <janahassan2052004@gmail.com>
2025-10-03 03:06:38 +0300

    added audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-} from "react-router-dom";
-import ExplorePage from "./pages/ExplorePage"; // landing
+import React, { useRef, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+
+import ExplorePage from "./pages/ExplorePage";
 import Navbar from "./components/Navbar";
 import DefendEarth from "./pages/DefendEarth";
 import AsteroidSimulation from "./pages/AsteroidSimulation";
@@ -16,21 +13,46 @@ function Layout() {
   return (
     <div className="min-h-screen bg-black text-white">
       <Navbar />
-      {/* <div className="p-6"> */}
-        <Outlet /> {/* where child route content appears */}
-      </div>
-    // </div>
+      <Outlet /> {/* child routes appear here */}
+    </div>
   );
 }
 
 function App() {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const playAudio = () => {
+      if (audioRef.current) {
+        audioRef.current.play().catch((err) => {
+          console.log("Autoplay blocked, waiting for user interaction", err);
+        });
+      }
+    };
+
+    // Try autoplay immediately
+    playAudio();
+
+    // Play on first user interaction if blocked
+    document.addEventListener("click", playAudio, { once: true });
+
+    return () => {
+      document.removeEventListener("click", playAudio);
+    };
+  }, []);
+
   return (
     <Router>
+      {/* Continuous background music */}
+      <audio ref={audioRef} loop>
+        <source src="/music/Sakta.mp3" type="audio/mpeg" />
+      </audio>
+
       <Routes>
-        {/* Landing page (no navbar) */}
+        {/* Landing page */}
         <Route path="/" element={<ExplorePage />} />
 
-        {/* All pages that need navbar */}
+        {/* Pages with navbar */}
         <Route element={<Layout />}>
           <Route path="defend-earth" element={<DefendEarth />} />
           <Route path="asteroid-simulation" element={<AsteroidSimulation />} />
